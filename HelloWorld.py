58d64da6a2a9f0bb6875266dda918943a4f5eac1
MyatSuYamone <msym2589@gmail.com>
2019-10-27 18:14:00 +0630

    Tax Calculation

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,19 @@
-print("Hello world")
+print("Hello World")
+
+#Sample
+charge = 15000
+tax = 50 / 1000
+total_tax = charge * tax
+total_paid = charge + total_tax
+total_paid = ?
+
+#Problem
+
+charge = 15000
+tax = 50 / 1000
+total_tax = charge * tax
+total_tax
+750.0
+total_paid = charge + total_tax
+total_paid
+15750.0
