69433ccd1adf63526408593a0ca8edd863cf5914
MyatSuYamone <msym2589@gmail.com>
2019-10-28 18:21:49 +0630

    Add HelloWorld

--- HelloWorld.py
+++ HelloWorld.py
@@ -17,3 +17,14 @@ total_tax
 total_paid = charge + total_tax
 total_paid
 15750.0
+
+#Problem2
+
+charge = 15000
+tax = 50/1000
+total_tax = charge * tax
+total_paid = charge + total_tax
+total_tax
+750.0
+total_paid
+15750.0
